8585f9ab123ff3de82383ef2714e3f803129368d
Ansel Rosenberg <ansel01@gmail.com>
2021-01-25 18:32:06 -0500

    add breaking API change

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,9 +3,9 @@ package fizzbuzz
 import "strconv"
 
 // Do gets output of FizzBuzz algorithm from 1-15 inclusive
-func Do() []string {
+func Do(n int) []string {
 	var out []string
-	for i := 1; i <= 15; i++ {
+	for i := 1; i <= n; i++ {
 		s := ""
 		if i%3 == 0 {
 			s += "Fizz"
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -10,7 +10,7 @@ func TestFizzBuzz(t *testing.T) {
 	expected := []string{
 		"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz",
 	}
-	actual := fizzbuzz.Do()
+	actual := fizzbuzz.Do(15)
 	if len(expected) != len(actual) {
 		t.Errorf("len(fizzbuzz.Do()) = %d, want %d", len(actual), len(expected))
 	}
